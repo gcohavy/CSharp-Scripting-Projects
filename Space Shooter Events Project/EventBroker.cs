b2380e5ef8a5cd4d43b18810ef52b17fb9e101a4
Gabriel Cohavy <gcohavy@gmail.com>
2020-12-22 11:06:34 -0800

    Added scripts for space shooter project

---
+++ Space Shooter Events Project/EventBroker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class EventBroker
+{
+    public static event Action ProjectileOutOfBounds;
+    public static void CallProjectileOutOfBounds()
+    {
+        if(ProjectileOutOfBounds != null)
+        {
+            ProjectileOutOfBounds();
+        }
+    }
+}
