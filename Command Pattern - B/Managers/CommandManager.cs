43ab9a062960c7ee1558f6dc8d47e61aaf1e0ad8
Gabriel Cohavy <gcohavy@gmail.com>
2020-12-23 18:07:20 -0800

    Second example of the command pattern in action

---
+++ Command Pattern - B/Managers/CommandManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CommandManager : MonoBehaviour
+{
+    private static CommandManager _instance;
+    public static CommandManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+                Debug.Log("CommandManger is NULL");
+            return _instance;
+        }
+    }
+
+    private List<ICommand> _commandBuffer = new List<ICommand>();
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void AddCommand(ICommand command)
+    {
+        _commandBuffer.Add(command);
+    }
+
+    public void Rewind()
+    {
+        StartCoroutine(ReverseRoutine());
+    }
+
+    IEnumerator ReverseRoutine()
+    {
+        Debug.Log("Rewinding...");
+        foreach(var command in Enumerable.Reverse(_commandBuffer))
+        {
+            command.Undo();
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("Finished.");
+    }
+
+    public void Play()
+    {
+        StartCoroutine(PlayRoutine());
+    }
+
+    IEnumerator PlayRoutine()
+    {
+        Debug.Log("Playing...");
+        foreach(var command in _commandBuffer)
+        {
+            command.Execute();
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("Finished.");
+    }
+}
